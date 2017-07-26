c91ea17f94d6184a4d4187e2601c85d77c207472
月砂 <windwingwood@qq.com>
2017-07-26 16:29:31 +0800

    开始向下一个功能进发。

--- clickCalculate/clickCalculate/AppDelegate.m
+++ clickCalculate/clickCalculate/AppDelegate.m
@@ -9,11 +9,13 @@
 #import "AppDelegate.h"
 #import "calculateView.h"
 #import <objc/runtime.h>
+#import <net/if.h>
 
 @interface AppDelegate ()
 
 @property (nonatomic, strong) NSStatusItem *statusItem;
 @property (nonatomic, strong) NSPopover *popover;
+@property (nonatomic, strong) calculateView *calView;
 @property (weak) IBOutlet NSMenu *mainMenu;
 
 
@@ -25,8 +27,6 @@
     // Insert code here to initialize your application
     [self setStatusBar];
 //    [self setRuntime];
-    //[self setMenu];
-    
 }
 
 
@@ -45,28 +45,24 @@
     
 }
 
-- (void)setMenu{
-    NSApplication * app = [NSApplication sharedApplication];
-    app.mainMenu = _mainMenu;
-}
-
 - (void)setStatusBar{
-    //初始化
-    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
+    //初始化 - 可变宽度
+    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     //添加事件
     _statusItem.target = self;
     _statusItem.button.action = @selector(clickItem:);
-    //设置图标
-    [_statusItem.button setImage:[NSImage imageNamed:@""]];
+    //设置标题
+    _statusItem.title = @"0kbs";
     //设置弹出页
     [self setPopover];
 }
 
 - (void)setPopover{
+    _calView = [calculateView new];
     _popover = [NSPopover new];
     _popover.behavior = NSPopoverBehaviorTransient;
     _popover.appearance = [NSAppearance appearanceNamed:NSAppearanceNameVibrantLight];
-    _popover.contentViewController = [calculateView new];
+    _popover.contentViewController = _calView;
 }
 
 - (void)clickItem:(NSStatusBarButton *)button{
